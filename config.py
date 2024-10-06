c550425bbb984bbcd6396ba342bf944c37294b71
Powermatrix12 <167418825+Powermatrix12@users.noreply.github.com>
2024-10-06 15:53:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,87 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7201537576:AAH3mjArvbpLpuFoyZ3Sw5mhi74Lc9RlGPQ")
+APP_ID = int(os.environ.get("APP_ID", "29236567"))
+API_HASH = os.environ.get("API_HASH", "7df329734bf9530a2ee73dcc06dd0df9")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001992443365"))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "5904478052"))
+PORT = os.environ.get("PORT", "8069")
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://zuuvou:XNWdDOsGFprx7Cnw@clusterop.pnyvj.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "clusterop")
+
+#Shortner (token system) 
+
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "a7bbb358e33d9962133d31e14129ab86af7c94ed")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/middlenightdiaries/10") 
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002042137942"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002001392051"))
+
+if FORCE_SUB_CHANNEL > FORCE_SUB_CHANNEL2:
+    temp = FORCE_SUB_CHANNEL2 
+    FORCE_SUB_CHANNEL2 = FORCE_SUB_CHANNEL
+    FORCE_SUB_CHANNEL = temp
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}, Thanks for using me :D @POWERMODOWNER ⚡️.")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "6706434927").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @POWERMODOWNER</b>")
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "Hᴍᴍ! Dᴏɴ'ᴛ ᴍᴇssᴀɢᴇ ᴍᴇ❌ I ᴅɪᴅɴ'ᴛ ʀᴇᴘʟʏ ʏᴏᴜ🤐"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(6497757690)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
